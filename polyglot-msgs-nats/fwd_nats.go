d559d8e75d95d4e7b71185bf49f96cefa3b0e865
mring33621 <matthew_s_ring@yahoo.com>
2016-12-20 15:17:07 -0600

    pretty much done

--- polyglot-msgs-nats/fwd_nats.go
+++ polyglot-msgs-nats/fwd_nats.go
@@ -2,22 +2,30 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"os"
 
 	"strings"
 
 	"github.com/jprichardson/readline-go"
+	"github.com/nats-io/go-nats"
 )
 
+func sendMsg(nc *nats.Conn, subject string, msgBody string) {
+	nc.Publish(subject, []byte(msgBody))
+}
+
 func main() {
+
+	nc, _ := nats.Connect(nats.DefaultURL)
+
 	var buff bytes.Buffer
 	readline.ReadLine(os.Stdin, func(line string) {
 		if strings.HasPrefix(line, "==> ") {
 			if buff.Len() > 0 {
 				buff.WriteString("********************end********************\n")
-				fmt.Println(buff.String())
+				msgBody := buff.String()
 				buff.Reset()
+				sendMsg(nc, "BigBrother", msgBody)
 			}
 			buff.WriteString("*******************start*********************\n")
 		}
@@ -26,6 +34,8 @@ func main() {
 	})
 	if buff.Len() > 0 {
 		buff.WriteString("********************end********************\n")
-		fmt.Println(buff.String())
+		sendMsg(nc, "BigBrother", buff.String())
 	}
+
+	nc.Close()
 }
--- polyglot-msgs-nats/src/main/java/com/mattring/blog/nats/post1/EmailWatcher.java
+++ polyglot-msgs-nats/src/main/java/com/mattring/blog/nats/post1/EmailWatcher.java
@@ -1,7 +1,13 @@
 package com.mattring.blog.nats.post1;
 
+import io.nats.client.Connection;
+import io.nats.client.Nats;
+
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.StreamSupport;
 
 /**
  * Created by mring on 12/11/2016.
@@ -9,10 +15,81 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class EmailWatcher {
 
     private static final String POISON = "EmailWatcher.Poison";
+    private static final String[] DEFAULT_KEYWORDS =
+            new String[]{
+                    "secret",
+                    "trade",
+                    "balance sheet",
+                    "legal",
+                    "audit",
+                    "account"
+            };
 
     private final BlockingQueue<String> inbox;
+    private String[] keywords;
 
     public EmailWatcher() {
         inbox = new LinkedBlockingQueue<>();
+    }
+
+    public void setKeywords(String[] keywords) {
+        this.keywords = keywords;
+    }
+
+    void enqueueMsg(String msg) {
+        try {
+            inbox.put(msg);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    String nextMsg() {
+        String nextItem = "";
+        try {
+            nextItem = inbox.take();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return nextItem;
+    }
+
+    boolean flagMsg(String msg) {
+        final String lcs = msg.toLowerCase();
+        return Arrays.stream(keywords).parallel().anyMatch(lcs::contains);
+    }
+
+    void alertSomeone(String msg) {
+        System.err.println(msg);
+    }
+
+    public void stop() {
+        enqueueMsg(POISON);
+    }
+
+    public void run() {
+
+        if (keywords == null || keywords.length == 0) {
+            keywords = DEFAULT_KEYWORDS;
+        }
+
+        // Connect to default URL ("nats://localhost:4222")
+        try (Connection nc = Nats.connect()) {
+            nc.subscribe("BigBrother", m -> {
+                enqueueMsg(new String(m.getData()));
+            });
+            StreamSupport.stream(
+                    new KillableSpliterator<String>(this::nextMsg, POISON), true)
+                    .filter(this::flagMsg)
+                    .forEach(this::alertSomeone);
+        } catch (IOException ioex) {
+            ioex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        EmailWatcher watcher = new EmailWatcher();
+        watcher.setKeywords(args);
+        watcher.run();
     }
 }
